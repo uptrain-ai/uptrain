22f218446b66094a5f86b556989c43a5de187fd9
vipgupta <vipgupta84@gmail.com>
2022-12-01 13:09:47 +0530

    Fixing minor bugs

--- oodles/__init__.py
+++ oodles/__init__.py
@@ -1,5 +1,5 @@
 from oodles.core.classes.framework import Framework
 from oodles.core.lib.decorators import monitor
 from oodles.core.classes.anomalies.signal import Signal
-from oodles.core.classes.anomalies.abstract_anomaly import AbstractAnomaly
+from oodles.core.classes.anomalies.custom_anomaly import CustomAnomaly
 from oodles.constants import ModelSignal, AnnotationMethod, Anomaly, DataDriftAlgo
--- oodles/core/classes/anomalies/custom_anomaly.py
+++ oodles/core/classes/anomalies/custom_anomaly.py
@@ -5,15 +5,15 @@ class CustomAnomaly(AbstractAnomaly):
     def __init__(self, check, log_args={}):
         self.dashboard_name = check.get('dashboard_name', 'custom_measure')
         super().__init__(log_args=log_args)
-        self.initialize_func = check.get('custom_initialize_func', None)
-        self.check_func = check['custom_check_func']
+        self.initialize_func = check.get('initialize_func', None)
+        self.check_func = check['check_func']
         self.need_gt = check['need_gt']
 
         if self.initialize_func is not None:
-            self.initialize_func()
+            self.initialize_func(self)
 
     def check(self, inputs, outputs, gts=None, extra_args={}):
-        return self.check_func(inputs, outputs, gts=gts, extra_args=extra_args)
+        return self.check_func(self, inputs, outputs, gts=gts, extra_args=extra_args)
 
     def is_data_interesting(self, inputs, outputs, gts=None, extra_args={}):
         return False
