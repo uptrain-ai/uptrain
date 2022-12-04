0dd6f19e56ae87e27dba28181ca7bbe965fa1574
AbinavRavi <abinav.ravi@tum.de>
2022-12-04 23:17:21 +0530

    used two metrics to determine covariate shifts

---
+++ examples/data_drifts/covariate_drift/drift_detection.py
@@ -0,0 +1,27 @@
+from scipy.stats import entropy
+from scipy.special import kl_div
+from scipy.spatial.distance import jensenshannon
+
+class Train:
+    def __init__(self) -> None:
+        pass
+
+    def save_features(self):
+        pass
+
+    def train(self):
+        pass
+
+class Inference:
+    def __init__(self) -> None:
+        pass
+
+    def load_model(self):
+        pass
+
+    def inference(self):
+        pass
+
+class CovariateDrift:
+    def __init__(self) -> None:
+        pass
