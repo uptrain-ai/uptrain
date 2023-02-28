cacc75a57e4ed0b70c8a7f024e9191dcd0bacd24
sourabhagr <108270398+sourabhagr@users.noreply.github.com>
2023-02-27 23:45:31 -0800

    Bug fix in norm ratio, works for scalars as well now (#119)
    * Bug fix in norm ratio, works for scalars as well now
    
    * Fix cosine distance also

--- uptrain/core/classes/distances/norm_ratio.py
+++ uptrain/core/classes/distances/norm_ratio.py
@@ -5,6 +5,12 @@ class NormRatio:
     def compute_distance(self, base, ref) -> float:
         base = np.array(base)
         ref = np.array(ref)
-        base_norm = np.linalg.norm(base, axis=1)
-        ref_norm = np.linalg.norm(ref, axis=1)
+        if len(base.shape) > 1:
+            base_norm = np.linalg.norm(base, axis=1)
+        else:
+            base_norm = np.abs(base)
+        if len(ref.shape) > 1:
+            ref_norm = np.linalg.norm(ref, axis=1)
+        else:
+            ref_norm = np.abs(ref)
         return base_norm / np.maximum(ref_norm, 1e-6)
