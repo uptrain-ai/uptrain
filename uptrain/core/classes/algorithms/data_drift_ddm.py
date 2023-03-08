5f1f980c413e6fbaedcaddbf4b9bf09c44316d78
a-r-r-o-w <avs050602@gmail.com>
2023-03-08 12:18:36 +0530

    make uptrain.core.classes.algorithms.DataDriftDDM API to stay consistent with previous versions

--- uptrain/core/classes/algorithms/data_drift_ddm.py
+++ uptrain/core/classes/algorithms/data_drift_ddm.py
@@ -4,4 +4,23 @@ from river import drift
 
 
 class DataDriftDDM(drift.DDM):
-    pass
+    def __init__(self, warm_start, warning_threshold, alarm_threshold):
+        super().__init__(warm_start, warning_threshold, alarm_threshold)
+
+        self.alert: str = None
+        self.total_count: int = 0
+
+        self._reset()
+
+    def _reset(self):
+        super()._reset()
+        self.alert = None
+
+    def add_prediction(self, prediction: int) -> str:
+        self.total_count += 1
+        self.update(prediction)
+
+        if self.drift_detected:
+            self.alert = f"Drift detected with DDM at time: {self.total_count}!!!"
+
+        return self.alert
