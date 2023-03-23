43bbd68fc47e50f619c83879480cb3aede2da6d2
a-r-r-o-w <avs050602@gmail.com>
2023-03-23 15:12:58 +0530

    hacky integration of Visual.PLOT to uptrain (requires further testing)

--- uptrain/core/classes/managers/check_manager.py
+++ uptrain/core/classes/managers/check_manager.py
@@ -16,7 +16,7 @@ from uptrain.core.classes.statistics import (
     Convergence,
     Distribution,
 )
-from uptrain.core.classes.visuals import DimensionalityReduction, UMAP_PRESENT, Shap, SHAP_PRESENT
+from uptrain.core.classes.visuals import DimensionalityReduction, UMAP_PRESENT, Shap, SHAP_PRESENT, Plot
 
 
 class CheckManager:
@@ -106,6 +106,9 @@ class CheckManager:
                 print(
                     """SHAP is not installed. For SHAP explainability, please install it by running `pip install shap matplotlib`."""
                 )
+        elif check["type"] == Visual.PLOT:
+            custom_monitor = Plot(self.fw, check)
+            self.visuals_to_check.append(custom_monitor)
         else:
             raise Exception("Visual type not Supported")
 
--- uptrain/core/classes/visuals/plot.py
+++ uptrain/core/classes/visuals/plot.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 from uptrain.core.classes.visuals import AbstractVisual
 from uptrain.constants import PlotType, Visual
 
@@ -6,18 +8,21 @@ class Plot(AbstractVisual):
     visual_type = Visual.PLOT
     dashboard_name = "Plot"
 
-    def base_init(self, framework, check) -> None:
+    def base_init(self, framework, check: Dict[str, Any]) -> None:
         self.framework = framework
         self.plot = check["plot"]
+        self.plot_name = check.get("plot_name", "Plot")
 
         if self.plot == PlotType.LINE_CHART:
-            x_feature_name = check.get('x_feature_name', None)
-            y_feature_name = check.get('y_feature_name', None)
+            self.x_feature_name = check.get('x_feature_name', None)
+            self.y_feature_name = check.get('y_feature_name', None)
 
-            if x_feature_name is None and y_feature_name is None:
+            if self.x_feature_name is None and self.y_feature_name is None:
                 raise Exception("Both x_feature_name and y_feature_name are None - atleast one must be not None")
         else:
             raise Exception("Plot Type is not supported")
     
     def base_check(self, inputs, outputs, gts=None, extra_args={}) -> None:
-        pass
+        # TODO: handle if x_feature_name or y_feature_name is None
+        for i in range(len(inputs[self.x_feature_name])):
+            self.framework.log_handler.add_scalars(self.plot_name, {"y_value": inputs[self.y_feature_name][i]}, inputs[self.x_feature_name][i], self.dashboard_name)
