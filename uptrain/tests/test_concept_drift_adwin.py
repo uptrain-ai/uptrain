513fa224ea765ea0a2b0a921404e65a5fd1b924e
a-r-r-o-w <avs050602@gmail.com>
2023-03-25 04:56:25 +0530

    improve concept_drift_adwin test

--- uptrain/tests/test_concept_drift_adwin.py
+++ uptrain/tests/test_concept_drift_adwin.py
@@ -5,13 +5,41 @@ import uptrain
 
 
 def test_concept_drift_adwin():
-    # Generate data for 4 distributions
     random_state = np.random.RandomState(seed=1337)
-    params = [(0.0, 0.03, 1000), (0.5, 0.08, 1000)]
+    
+    # We generate 2 distributions:
+    #   - The first distribution contain 1000 values with mean 0.0 and standard deviation 0.03
+    #   - The second distribution contain 1000 values with mean 0.5 and standard deviation 0.08
+    #
+    # To maintain the simplicity of this test, we will assume that there is a hypothetical model
+    # that predicts the output is the same as input. However, this is not the case.
+    # In reality:
+    #   - the ground truth for the first distribution is 0.0
+    #   - the ground truth for the first half of second distribution is 0.5
+    #   - the ground truth for the second half of second distribution is 0.6
+    #
+    # This means that between the model predictions and ground truths, a concept drift will arise.
+    # The drift will arise somewhere around the 1500th value.
+    #
+    # Why?
+    #  - The model will predict values approximately equal to 0.0 for the first distribution.
+    #    This is correct.
+    #  - The model will predict values approximately equal to 0.5 for the first half of the
+    #    second distribution. This is correct.
+    #  - The model will predict values approximately equal to 0.5 for the second half of the
+    #    second distribution. This is incorrect.
+    #
+    # The concept drift will be detected by the ADWIN algorithm.
+
+    n = 1000
+    params = [(0.0, 0.03, n), (0.5, 0.08, n)]
     distributions = np.array([random_state.normal(*param) for param in params])
+    
+    ground_truths = np.array([0.0] * n + [0.5] * (n // 2) + [0.6] * (n // 2))
     stream = distributions.flatten()
 
     def plot_data(drifts=None):
+        """Helper function to plot data."""
         _ = plt.figure(figsize=(7, 3), tight_layout=True)
         gridspecs = gridspec.GridSpec(1, 2, width_ratios=[3, 1])
 
@@ -33,13 +61,15 @@ def test_concept_drift_adwin():
     # Plot data for debugging
     # plot_data()
 
+    # Create a configuration for the framework
     cfg = {
+        # Define checks to be performed
         "checks": [
             {
                 "type": uptrain.Monitor.CONCEPT_DRIFT,
                 "algorithm": uptrain.DataDriftAlgo.ADWIN,
                 "measurable_args": {
-                    "type": uptrain.MeasurableType.INPUT_FEATURE,
+                    "type": uptrain.MeasurableType.MAE,
                     "feature_name": "data",
                 },
                 "delta": 0.002,
@@ -49,7 +79,11 @@ def test_concept_drift_adwin():
                 "grace_period": 5,
             }
         ],
+        
+        # Specify where the logging data should be stored
         "retraining_folder": "uptraining_smart_data_concept_drift_adwin",
+
+        # True if we want streamlit logging, False otherwise
         "logging_args": {"st_logging": True},
     }
 
@@ -59,6 +93,7 @@ def test_concept_drift_adwin():
 
     for i in range(size // batch_size):
         inputs = {"data": stream[i * batch_size : (i + 1) * batch_size]}
-        ids = framework.log(inputs=inputs)
-        gts = [0] * batch_size
+        outputs = stream[i * batch_size : (i + 1) * batch_size]
+        gts = ground_truths[i * batch_size : (i + 1) * batch_size]
+        ids = framework.log(inputs=inputs, outputs=outputs)
         framework.log(identifiers=ids, gts=gts)
