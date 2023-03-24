7a5d06136ae5646aa9c0a0574db0e8a7952b45f8
a-r-r-o-w <avs050602@gmail.com>
2023-03-25 03:17:28 +0530

    add bar chart test

--- uptrain/tests/test_plot.py
+++ uptrain/tests/test_plot.py
@@ -1,18 +1,40 @@
 import random
 import uptrain
 
-n = 100
-epochs = list(range(n))
-loss = [random.randint(0, 100) for _ in range(n)]
+# For line chart
+line_n = 100
+epoch = list(range(line_n))
+loss = [random.randint(0, 100) for _ in range(line_n)]
+
+# For bar chart
+bar_n = 10
+bar1_y = [i for i in range(bar_n)]
+bar1_x = ["date: " + str(i) for i in range(bar_n)]
+
+bar2_y = [i for i in range(bar_n - 1, -1, -1)]
+bar2_x = ["date: " + str(i) for i in range(bar_n)]
 
 cfg = {
     "checks": [
         {
             "type": uptrain.Visual.PLOT,
             "plot": uptrain.PlotType.LINE_CHART,
-            "x_feature_name": "epochs",
+            "x_feature_name": "epoch",
             "y_feature_name": "loss",
-            "plot_name": "this is da line chart"
+            "plot_name": "epoch vs loss 1"
+        },
+        {
+            "type": uptrain.Visual.PLOT,
+            "plot": uptrain.PlotType.LINE_CHART,
+            "y_feature_name": "loss",
+            "plot_name": "epoch vs loss 2"
+        },
+        {
+            "type": uptrain.Visual.PLOT,
+            "plot": uptrain.PlotType.BAR_CHART,
+            "x_feature_name": "key",
+            "y_feature_name": "value",
+            "plot_name": "this is the bar chart"
         }
     ],
     "logging_args": {"st_logging": True}
@@ -20,4 +42,6 @@ cfg = {
 
 framework = uptrain.Framework(cfg)
 
-framework.log({"epochs": epochs, "loss": loss})
+framework.log({"epoch": epoch, "loss": loss})
+framework.log({"bars": ["bar1"] * bar_n, "key": bar1_x, "value": bar1_y})
+framework.log({"bars": ["bar2"] * bar_n, "key": bar2_x, "value": bar2_y})
