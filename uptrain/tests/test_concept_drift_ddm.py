b5aeee976f094b1a915f369537460e6d42e586e9
a-r-r-o-w <avs050602@gmail.com>
2023-03-26 05:12:30 +0530

    rewrite concept_drift_ddm test synthetically

--- uptrain/tests/test_concept_drift_ddm.py
+++ uptrain/tests/test_concept_drift_ddm.py
@@ -1,102 +1,96 @@
-import os
-import subprocess
-
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+import numpy as np
+import string
 import uptrain
-from xgboost import XGBClassifier
 
 
 def test_concept_drift_ddm():
-    data_file = "NSL_KDD_binary.csv"
-    remote_url = "https://oodles-dev-training-data.s3.amazonaws.com/NSL_KDD_binary.csv"
+    """Test concept drift detection using DDM algorithm.
 
-    if not os.path.exists(data_file):
-        try:
-            # Most Linux distributions have Wget installed by default.
-            # Below command is to install wget for MacOS
-            wget_installed_ok = subprocess.call(
-                "brew install wget",
-                shell=True,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.STDOUT,
-            )
-            print("Successfully installed wget")
-        except:
-            pass
+    We generate a synthetic dataset consisting of 1000 strings, each of length
+    between 10 to 20 characters, containing lowercase alphabets, digits, and
+    spaces. We also generate a binary sentiment label for each string. A label
+    of 0 means that the sentiment is negative, and a label of 1 means that the
+    sentiment is positive.
 
-        try:
-            if not os.path.exists("data.zip"):
-                file_downloaded_ok = subprocess.call(
-                    "wget " + remote_url,
-                    shell=True,
-                    stdout=subprocess.DEVNULL,
-                    stderr=subprocess.STDOUT,
-                )
-                print("Data downloaded")
-        except:
-            print("Could not load training data")
-            print("Please follow following steps to manually download data")
-            print(
-                "Step 1: Open in browser: https://oodles-dev-training-data.s3.amazonaws.com/NSL_KDD_binary.csv"
-            )
-            print(
-                "Step 2: Download and move the file to example location (i.e. uptrain/examples/2_cyber_attack_classification/"
-            )
-    else:
-        print("Data file exists. Skipping download.")
+    Note that you can use a real dataset as well. However, for the sake of
+    simplicity, we use a synthetic dataset to demonstrate how DDM works.
 
-    df = pd.read_csv(data_file)
-    X = df.iloc[:, :-1].values
-    y = df.iloc[:, -1].values
+    Assume that we have a model that has been trained to predict the sentiment
+    of a sentence. It performs well for the most part but occasionally makes
+    incorrect predictions. However, for some reason, it starts to perform poorly
+    on some newer data. How do we detect this?
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, train_size=0.1, test_size=0.9, random_state=0, shuffle=False
+    We use the DDM algorithm to detect the concept drift. The drift will arise
+    somewhere around the 800th sentence.
+
+    To introduce concept drift to our synthetic dataset, we randomly flip the
+    predictions of 10% of the strings. This simulates a scenario where the model
+    makes incorrect predictions on some data. We also deliberately make the last
+    20% of predictions random. This simulates a scenario where the model encounters
+    new or unseen data, causing the predictions to become inaccurate.
+
+    In this test, we use the UpTrain framework to log the inputs, outputs, and ground
+    truths. We set up the framework to use the DDM algorithm to monitor the error rate
+    of the model. The error rate is calculated as the binary accuracy between the
+    predictions and the ground truths. We set the threshold for warning to 2.0, and
+    drift to 3.0. This means that if the error rate crosses 2.0, the model has
+    begun to perform poorly on some data. If the error rate crosses 3.0, a concept
+    drift alert will be raised. This means that the model is performing really poorly
+    and is inaccurate on some recent data that it has been exposed to.
+
+    The test passes if the DDM algorithm successfully detects the concept drift and
+    raises an alert around the point where the predictions start becoming inaccurate.
+    """
+    random_state = np.random.RandomState(seed=1337)
+    alphabet = np.array(list(map(ord, string.ascii_lowercase + string.digits + " ")))
+
+    def random_string(length):
+        return "".join(list(map(chr, random_state.choice(alphabet, size=length))))
+
+    n = 1000
+    inputs = np.array([random_string(random_state.randint(10, 20)) for _ in range(n)])
+    sentiments = np.array([random_state.choice([0, 1]) for _ in range(n)])
+    predictions = sentiments.copy()
+
+    # Make 10% predictions incorrect randomly, i.e., if a sentiment is correctly classified,
+    # make it incorrectly classified and vice versa
+    incorrect_predictions = random_state.choice(n, size=int(n * 0.1), replace=False)
+    predictions[incorrect_predictions] = 1 - predictions[incorrect_predictions]
+
+    # Deliberately make the last 20% of predictions random
+    random_predictions = np.arange(n - int(n * 0.2), n)
+    predictions[random_predictions] = random_state.choice(
+        [0, 1], size=len(random_predictions)
     )
 
-    # Train the XGBoost classifier with training data
-    classifier = XGBClassifier()
-    classifier.fit(X_train, y_train)
-
-    y_pred = classifier.predict(X_train)
-    print("Training accuracy: " + str(100 * accuracy_score(y_train, y_pred)))
-
-    """
-    Defining a custom drift metric where
-    the user just want to check if accuracy
-    drops beyond a threshold.
-    """
-
-    checks = [
-        {
-            "type": uptrain.Monitor.CONCEPT_DRIFT,
-            "algorithm": uptrain.DataDriftAlgo.DDM,
-            "warn_threshold": 2,
-            "alarm_threshold": 3,
-        },
-    ]
-
+    # Create a configuration for the framework
     cfg = {
-        # Checks to identify concept drift
-        "checks": checks,
-        # Folder that stores data logged by UpTrain
-        "retraining_folder": "uptrain_smart_data",
+        # Define checks to be performed
+        "checks": [
+            {
+                "type": uptrain.Monitor.CONCEPT_DRIFT,
+                "algorithm": uptrain.DataDriftAlgo.DDM,
+                "measurable_args": {
+                    "type": uptrain.MeasurableType.ACCURACY,
+                    "feature_name": "data",
+                },
+                "warm_start": 200,
+                "warn_threshold": 2.0,
+                "alarm_threshold": 3.0,
+            }
+        ],
+        # Specify where the logging data should be stored
+        "retraining_folder": "uptraining_smart_data_concept_drift_ddm",
+        # True if we want streamlit logging, False otherwise
+        "logging_args": {"st_logging": True},
     }
 
-    # Initialize the UpTrain framework
     framework = uptrain.Framework(cfg)
-    batch_size = len(X_test)
+    batch_size = 64
 
-    for i in range(int(len(X_test) / batch_size)):
-        # Do model prediction
-        inputs = {"feats": X_test[i * batch_size : (i + 1) * batch_size]}
-        preds = classifier.predict(inputs["feats"])
-
-        # Log model inputs and outputs to monitor concept drift
-        ids = framework.log(inputs=inputs, outputs=preds)
-
-        # Attach ground truth to corresponding predictions
-        # in UpTrain framework and identify concept drift
-        ground_truth = y_test[i * batch_size : (i + 1) * batch_size]
-        framework.log(identifiers=ids, gts=ground_truth)
+    for i in range(n // batch_size):
+        input = {"data": inputs[i * batch_size : (i + 1) * batch_size]}
+        output = predictions[i * batch_size : (i + 1) * batch_size]
+        gts = sentiments[i * batch_size : (i + 1) * batch_size]
+        ids = framework.log(inputs=input, outputs=output)
+        framework.log(identifiers=ids, gts=gts)
