502ed1bf96aee27c6d90562a68da556b89a89da4
a-r-r-o-w <avs050602@gmail.com>
2023-03-27 04:36:41 +0530

    run black for tests

--- uptrain/tests/test_algorithms_popularity_bias.py
+++ uptrain/tests/test_algorithms_popularity_bias.py
@@ -7,13 +7,13 @@ from uptrain.core.classes.algorithms.popularity_bias import PopularityBias
 @pytest.fixture
 def pb():
     sessions = [
-        ['a', 'b', 'c'],
-        ['a', 'b'],
-        ['a', 'c'],
-        ['b', 'c'],
-        ['b', 'c'],
-        ['b'],
-        ['c'],
+        ["a", "b", "c"],
+        ["a", "b"],
+        ["a", "c"],
+        ["b", "c"],
+        ["b", "c"],
+        ["b"],
+        ["c"],
     ]
     pb = PopularityBias(sessions=sessions)
     return pb
@@ -21,38 +21,37 @@ def pb():
 
 def test_init(pb: PopularityBias):
     """Test that the PopularityBias object is initialized correctly.
-    
+
     The test passes if the PopularityBias object is initialized correctly
     and the popularity map is estimated correctly.
     """
 
-    assert len(pb.all_popularity) == 0
-
     expected_popmap = {
-        'a': 0.230760,
-        'b': 0.384610,
-        'c': 0.384610,
+        "a": 0.230760,
+        "b": 0.384610,
+        "c": 0.384610,
     }
-    keys = ['a', 'b', 'c']
+    keys = ["a", "b", "c"]
     result = np.array([pb.pop_map[key] for key in keys])
     expected_result = np.array([expected_popmap[key] for key in keys])
-    
+
+    assert len(pb.all_popularity) == 0
     assert np.allclose(result, expected_result, rtol=1e-4)
 
 
 def test_add_prediction(pb: PopularityBias):
     """Test that the PopularityBias object adds predictions correctly.
-    
+
     The test passes if the PopularityBias object adds predictions correctly
     and the popularity scores are added correctly.
     """
 
-    pb.add_prediction(['a', 'b'])
-    pb.add_prediction(['b', 'c'])
-    
+    pb.add_prediction(["a", "b"])
+    pb.add_prediction(["b", "c"])
+
     assert len(pb.all_popularity) == 4
-    
+
     result = pb.all_popularity
     expected_result = [0.230760, 0.384610, 0.384610, 0.384610]
-    
+
     assert np.allclose(result, expected_result, rtol=1e-4)
