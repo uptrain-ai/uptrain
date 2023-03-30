220d0c3df8316948b6e958323eb1f2efd31aff0a
Dhruv Chawla <43818888+Dominastorm@users.noreply.github.com>
2023-03-30 10:32:55 +0530

    Update uptrain.core.classes.distances (#188)
    * Update l2_distance.py
    
    * Add compatibility check to norm ratio
    
    * Refactor NORM_MIN
    
    * Abstract compatibility check
    
    * Abstract norm calculation
    
    * run black
    
    * Rename NORM_MIN -> norm_min
    
    * Remove function overriding
    
    * Rename abstract.py
    
    * Add Hamming Distance
    
    * Fix Hamming Distance
    
    * Remove calculate_norm from abstract class
    
    * Add option to normalize hamming distance

--- uptrain/core/classes/distances/abstract_distance.py
+++ uptrain/core/classes/distances/abstract_distance.py
@@ -1,10 +1,16 @@
 from abc import ABC
 from typing import Any
 
+import numpy as np
+
 
 class AbstractDistance(ABC):
     def __init__(self) -> None:
         super().__init__()
-    
+
     def compute_distance(self, base, reference) -> Any:
         raise Exception("Distance computation should be defined for each subclass")
+
+    def check_compatibility(self, base, reference) -> None:
+        if base.shape != reference.shape:
+            raise Exception("Incompatible shapes for base and reference")
