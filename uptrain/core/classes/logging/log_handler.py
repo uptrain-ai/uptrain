79fe35745f1edab19ce495cdf4e0ab9c2578e552
Ishan Anand <anand.ishan@outlook.com>
2023-04-17 13:21:15 +0530

    checkpoint

--- uptrain/core/classes/logging/log_handler.py
+++ uptrain/core/classes/logging/log_handler.py
@@ -1,14 +1,21 @@
 import os
 import shutil
 import urllib3
+import re
 import json
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from uptrain.core.classes.framework import Framework
+    from uptrain.core.classes.helpers.config_handler import Config
+
 
 class LogHandler:
-    def __init__(self, framework=None, cfg=None):
+    def __init__(self, framework: Framework, cfg: Config):
         self.fw = framework
-        log_folder = cfg.logging_args.log_folder
         self.path_all_data = framework.path_all_data
 
+        log_folder = cfg.logging_args.log_folder
         if os.path.exists(log_folder):
             print("Deleting the folder: ", log_folder)
             shutil.rmtree(log_folder)
@@ -19,27 +26,17 @@ class LogHandler:
 
             self.st_log_folder = os.path.join(log_folder, "st_data")
             os.makedirs(self.st_log_folder, exist_ok=True)
-            self.st_writer = StreamlitLogs(self.st_log_folder, port=cfg.logging_args.dashboard_port)
+            self.st_writer = StreamlitLogs(
+                self.st_log_folder, port=cfg.logging_args.dashboard_port
+            )
+
+            # serialize the config to a json file so the streamlit dashboard can read it
+            cfg_file = os.path.join(self.st_log_folder, "config.json")
+            with open(cfg_file, "w") as f:
+                json.dump(cfg.json(), f)
 
         # Get Webhook URL for alerting on slack
         self.webhook_url = cfg.logging_args.slack_webhook_url
-
-        # Saving config to get model metadata
-        self.cfg_metadata = {}
-        if len(cfg.checks) > 0:
-            check = cfg.checks[0]
-            self.add_st_metadata({'model_args': check.get('model_args', None), 
-                                  'feature_args': check.get('feature_args', None)})
-        else:
-            self.add_st_metadata({'model_args': None, 'feature_args': None})
-
-    def add_st_metadata(self, new_dict):
-        if self.st_writer is None:
-            return
-        self.cfg_metadata.update(new_dict)
-        metadata_file = os.path.join(self.st_log_folder, "metadata.json")
-        with open(metadata_file, "w") as f:
-            json.dump(self.cfg_metadata, f)
 
     def get_plot_save_name(self, plot_name, dashboard_name):
         if self.st_writer:
@@ -49,12 +46,20 @@ class LogHandler:
         else:
             return ""
 
-    def add_scalars(self, plot_name, dictn, count, dashboard_name, features={}, models={}, file_name=None, update_val=False):
+    def add_scalars(
+        self,
+        plot_name,
+        dictn,
+        count,
+        dashboard_name,
+        features={},
+        models={},
+        file_name=None,
+        update_val=False,
+    ):
         if self.st_writer is None:
             return
-        dashboard_name, plot_name = self.dir_friendly_name(
-            [dashboard_name, plot_name]
-        )
+        dashboard_name, plot_name = self.dir_friendly_name([dashboard_name, plot_name])
         dictn.update(features)
         dictn.update(models)
         new_dictn = dict(
@@ -69,26 +74,34 @@ class LogHandler:
         new_dictn.update({"x_count": count})
         if file_name is None:
             file_name = plot_name
-        self.st_writer.add_scalars(new_dictn, plot_folder, file_name=file_name, update_val=update_val)
-
-    def add_histogram(self, plot_name, data, dashboard_name, features=None, models=None, file_name=None):
-        dashboard_name, plot_name = self.dir_friendly_name(
-            [dashboard_name, plot_name]
+        self.st_writer.add_scalars(
+            new_dictn, plot_folder, file_name=file_name, update_val=update_val
         )
+
+    def add_histogram(
+        self,
+        plot_name,
+        data,
+        dashboard_name,
+        features=None,
+        models=None,
+        file_name=None,
+    ):
+        dashboard_name, plot_name = self.dir_friendly_name([dashboard_name, plot_name])
         if self.st_writer:
             dashboard_dir = os.path.join(self.st_log_folder, dashboard_name)
             plot_folder = os.path.join(dashboard_dir, "histograms", plot_name)
             os.makedirs(plot_folder, exist_ok=True)
             if file_name is None:
                 file_name = plot_name
-            self.st_writer.add_histogram(data, plot_folder, features=features, models=models, file_name=file_name)
+            self.st_writer.add_histogram(
+                data, plot_folder, features=features, models=models, file_name=file_name
+            )
 
     def add_bar_graphs(self, plot_name, data, dashboard_name, count=-1, hover_data={}):
         if self.st_writer is None:
             return
-        dashboard_name, plot_name = self.dir_friendly_name(
-            [dashboard_name, plot_name]
-        )
+        dashboard_name, plot_name = self.dir_friendly_name([dashboard_name, plot_name])
         dashboard_dir = os.path.join(self.st_log_folder, dashboard_name)
         plot_folder = os.path.join(dashboard_dir, "bar_graphs", plot_name)
         os.makedirs(plot_folder, exist_ok=True)
@@ -102,16 +115,7 @@ class LogHandler:
         return new_arr
 
     def convert_str(self, txt):
-        txt = txt.replace("(", "_")
-        txt = txt.replace(")", "_")
-        txt = txt.replace(":", "_")
-        txt = txt.replace(" ", "_")
-        txt = txt.replace(",", "_")
-        txt = txt.replace(">", "_")
-        txt = txt.replace("<", "_")
-        txt = txt.replace("=", "_")
-        txt = txt.replace("-", "_")
-        return txt
+        return re.sub(r"[^a-zA-Z0-9_]", "_", txt)
 
     def add_alert(self, alert_name, alert, dashboard_name):
         if self.st_writer:
@@ -122,17 +126,21 @@ class LogHandler:
             self.st_writer.add_alert(alert_name, alert, plot_folder)
 
         if self.webhook_url:
-            message = f"Dashboard: {dashboard_name}, Alert name: {alert_name}, Alert: {alert}"
-            self.slack_notification({'text': message})
+            message = (
+                f"Dashboard: {dashboard_name}, Alert name: {alert_name}, Alert: {alert}"
+            )
+            self.slack_notification({"text": message})
 
     def slack_notification(self, message):
         try:
             http = urllib3.PoolManager()
-            response = http.request('POST',
-                                    self.webhook_url,
-                                    body = json.dumps(message),
-                                    headers = {'Content-Type': 'application/json'},
-                                    retries = False)
+            response = http.request(
+                "POST",
+                self.webhook_url,
+                body=json.dumps(message),
+                headers={"Content-Type": "application/json"},
+                retries=False,
+            )
         except Exception as e:
             print("Caught Exception")
             print(e)
