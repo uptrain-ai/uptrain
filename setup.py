40d73eac4b301b50307800a09a4ab7281643fa0a
Vipul Gupta <vipgupta84@gmail.com>
2023-05-09 12:35:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="uptrain",
-    version="0.1.1",
+    version="0.1.2",
     description="UpTrain - ML Observability and Retraining Framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
