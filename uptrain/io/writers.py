9f51e879892b75dac87e578b1702121656cc2dd9
sourabhagr <108270398+sourabhagr@users.noreply.github.com>
2023-06-02 15:50:25 +0530

    Add testing config file for developer docs testing (#260)

--- uptrain/io/writers.py
+++ uptrain/io/writers.py
@@ -4,6 +4,7 @@ import typing as t
 from pydantic import BaseModel
 import polars as pl
 import deltalake as dl
+from pathlib import Path
 
 from uptrain.operators.base import *
 if t.TYPE_CHECKING:
@@ -37,3 +38,30 @@ class DeltaWriterExecutor(OperatorExecutor):
             self.columns = list(data.columns)
         assert set(self.columns) == set(data.columns)
         dl.write_deltalake(self.op.fpath, data.to_arrow())
+
+
+@register_op
+class JsonWriter(BaseModel):
+    fpath: str
+    columns: t.Optional[list[str]] = None
+
+    def make_executor(self, settings: t.Optional[Settings] = None):
+        return JsonWriterExecutor(self)
+
+
+class JsonWriterExecutor(OperatorExecutor):
+    op: JsonWriter
+    columns: t.Optional[list[str]] = None
+
+    def __init__(self, op: JsonWriter):
+        self.op = op
+        self.columns = list(self.op.columns) if self.op.columns is not None else None
+
+    def run(self, data: pl.DataFrame):
+        if self.columns is None:
+            self.columns = list(data.columns)
+        assert set(self.columns) == set(data.columns)
+
+        #TODO: There should be a better way to create folders than below
+        Path(self.op.fpath.split('.')[0]).mkdir(parents=True, exist_ok=True)
+        data.write_ndjson(file=self.op.fpath)
---
+++ uptrain/operators/language/not_implemented.py
@@ -0,0 +1,32 @@
+class Embeddings:
+    dummy = 1
+    def __init__(self, schema_data) -> None:
+        dummy = 2
+
+class Distribution:
+    dummy = 1
+    def __init__(self, schema_data, operation) -> None:
+        dummy = 2
+
+class CustomCheck:
+    dummy = 1
+
+class TextLength:
+    dummy = 1
+    def __init__(self, schema_data) -> None:
+        dummy = 2
+
+class Rogue:
+    dummy = 1
+    def __init__(self, schema_data) -> None:
+        dummy = 2
+
+class CosineSimilarity:
+    dummy = 1
+    def __init__(self, schema_data) -> None:
+        dummy = 2
+
+class UMAP:
+    dummy = 1    
+    def __init__(self, schema_data) -> None:
+        dummy = 2
