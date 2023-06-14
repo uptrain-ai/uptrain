c1d6b48f1b7b2d2f81ee7934c54835bcaf7305b6
sourabhagr <sourabh.agrawal031994@gmail.com>
2023-06-14 12:09:29 +0530

    Allow reading directly from postgres table as well as logging to postgres

--- uptrain/core/classes/io/runtime_manager.py
+++ uptrain/core/classes/io/runtime_manager.py
@@ -1,9 +1,21 @@
 import os
-from google.cloud import bigquery
 from datetime import datetime, timedelta
 import time
+import pandas as pd
+from uptrain.core.lib.helper_funcs import dependency_required
+
+try:
+    from google.cloud import bigquery
+except:
+    bigquery = None
+
+try:
+    import psycopg2
+except:
+    psycopg2 = None
 
 
+@dependency_required(bigquery, "'google-cloud-bigquery[pandas]'")
 class BigQueryReader:
     def __init__(self, args) -> None:
         self.sql_query = args.sql_query
@@ -25,6 +37,29 @@ class BigQueryReader:
         return df
 
 
+@dependency_required(psycopg2, "psycopg2")
+class PostgreSQLReader:
+    def __init__(self, args) -> None:
+        self.sql_query = args.sql_query
+        self.conn = psycopg2.connect(
+            database=args.database,
+            user=os.environ['USERNAME'],
+            password=os.environ['PASSWORD'])
+        self.sql_variables = args.sql_variables_dictn
+
+    def run(self, time_diff):
+        run_dict = {}
+        for var, val in self.sql_variables.items():
+            run_dict.update({
+                var: str(val + timedelta(seconds=time_diff))
+            })
+
+        query = self.sql_query.format(**run_dict)
+        print(f"Executing SQL query: {query} \n at UTC time: {str(datetime.utcnow())}")
+        df = pd.read_sql(query, self.conn)
+        print(f"Got {len(df)} rows")
+        return df
+
 
 class RunTimeManager:
     def __init__(self, args, fw) -> None:
@@ -41,7 +76,8 @@ class RunTimeManager:
             if datetime.utcnow() > (latest_run_time + timedelta(seconds=self.frequency_in_seconds)):
                 latest_run_time = latest_run_time + timedelta(seconds=self.frequency_in_seconds)
                 data = self.reader.run(self.time_diff)
-                self.framework.log(data)
+                if len(data) > 0:
+                    self.framework.log(data)
                 self.time_diff += self.frequency_in_seconds
             else:
                 time.sleep(self.frequency_in_seconds * 0.1)
@@ -50,5 +86,7 @@ class RunTimeManager:
     def _resolve(self, args):
         if args.type == 'bigquery':
             return BigQueryReader(args)
+        elif args.type == 'postgresql':
+            return PostgreSQLReader(args)
         else:
             raise Exception(f"{args.type} is not supported!")
