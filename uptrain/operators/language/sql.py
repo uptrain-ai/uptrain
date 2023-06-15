9a2c7c89771a9f5baeb6972541452e38ed6a09b7
Bhanu Renukuntla <bhanuvikasr@gmail.com>
2023-06-14 20:36:27 -0700

    support reading database schema

--- uptrain/operators/language/sql.py
+++ uptrain/operators/language/sql.py
@@ -3,6 +3,8 @@ Implement oeprators over text data.
 """
 
 from __future__ import annotations
+
+import os
 import re
 import typing as t
 from urllib.parse import urlparse
@@ -15,7 +17,48 @@ if t.TYPE_CHECKING:
     from uptrain.framework.config import *
 from uptrain.operators.base import *
 
-__all__ = ["HasStar"]
+__all__ = ["HasStar", "GetSchemaDefinition"]
+
+
+# operators to read schema definition from Spider sql dataset
+@register_op
+class GetSchemaDefinition(BaseModel):
+    col_in_schema: str = "schema"
+    col_out: str = "schema_def"
+
+    def make_executor(self, settings: t.Optional[Settings] = None):
+        return GetSchemaDefinitionExecutor(self, settings)
+
+
+class GetSchemaDefinitionExecutor(OperatorExecutor):
+    op: GetSchemaDefinition
+
+    def __init__(self, op: GetSchemaDefinition, settings: t.Optional[Settings] = None):
+        self.op = op
+
+    def __read_schema_definition(self, schema_name) -> str:
+        # TODO figure out a better way to set this
+        resources_path = "/Users/bhanu/src/uptrain_experiments/llm/spider/database"
+        # List to store all CREATE TABLE statements
+        create_table_statements = []
+
+        with open(os.path.join(resources_path, f'{schema_name}/schema.sql'), 'r') as file:
+            content = file.read()
+
+            # SQL statements are separated by ';'
+            statements = content.split(';')
+
+            # Create table statements
+            for statement in statements:
+                if statement.strip().startswith('CREATE TABLE'):
+                    # Add the statement to the list
+                    create_table_statements.append(statement.strip())
+        return "\n\n".join(create_table_statements)
+
+    def run(self, data: pl.DataFrame) -> TYPE_OP_OUTPUT:
+        schema_names = data.get_column(self.op.col_in_schema)
+        results = [self.__read_schema_definition(schema_name) for schema_name in schema_names]
+        return {"output": data.with_columns([pl.Series(self.op.col_out, results)])}
 
 
 # Check if SQL has star
