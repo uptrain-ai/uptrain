675a2b9157ea7ffa94fdbbb346a6f36ee68c4764
Dhruv Chawla <43818888+Dominastorm@users.noreply.github.com>
2023-07-05 17:29:07 +0530

    Add Evaluation and OpenAI evals tutorials to documentation (#295)
    * Update docs
    
    * Update prompt_experiments_tutorial.ipynb
    
    * Update prompt experiment example
    
    * Update prompt-experiments.mdx
    
    * Update prompt-experiments.mdx
    
    * Update prompt-experiments.mdx
    
    * Update tutorial
    
    * Update quickstart.mdx
    
    * Update quickstart.mdx
    
    * Update quickstart.mdx
    
    * Rename examples to tutorials
    
    * Update tutorials and add to documentation
    
    * Update openai-evals.mdx
    
    * Update validation.mdx
    
    * Update validation.mdx
    
    * Update documentation titles

--- validation_wrapper/validation_manager.py
+++ validation_wrapper/validation_manager.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 import typing as t
 import polars as pl
+from loguru import logger
 
 from uptrain.framework.signal import Signal
 from uptrain.framework.checks import Check, Settings
@@ -8,7 +9,7 @@ from uptrain.framework.checks import Check, Settings
 
 class ValidationManager(BaseModel):
     check: t.Any
-    completion_fn: t.Any
+    completion_function: t.Any
     pass_condition: t.Any
     max_retries: int = 3
 
@@ -21,14 +22,18 @@ class ValidationManager(BaseModel):
         response_is_valid = False
         num_tries = 0
         while not response_is_valid:
-            response = self.completion_fn(inputs)
+            num_tries += 1
+            response = self.completion_function(inputs)
             inputs["response"] = [response]
             data = pl.from_dict(inputs)
             data = self.check.run(data)
             response_is_valid = self.pass_condition.run(data)[0]
 
-            num_tries += 1
-            if num_tries > self.max_retries:
+            if response_is_valid:
+                logger.success(f"Validation check PASSED after {num_tries} attempt(s)")                
+            elif num_tries < self.max_retries:
+                logger.warning(f"RETRYING validation check {num_tries} of {self.max_retries}")
+            else:
                 response_is_valid = True
-
+                logger.warning(f"Validation check FAILED after {self.max_retries} attempt(s)")
         return response
