8e70d879d858a55f6e1c9319ed489ff1602e6e13
Ishan Anand <anand.ishan@outlook.com>
2023-07-08 11:15:22 +0530

    ananis/managed (#316)
    * add directive to include stub files
    
    * add manifest file
    
    * change to remote api

--- uptrain/framework/remote.py
+++ uptrain/framework/remote.py
@@ -5,7 +5,6 @@ on the UpTrain server.
 """
 # TODO: add user defined custom evaluations (maybe ship the full code to the server?)
 
-from dataclasses import dataclass
 import typing as t
 
 from loguru import logger
@@ -15,19 +14,40 @@ from uptrain.framework.checks import CheckSet
 from uptrain.framework.base import Settings
 
 
-@dataclass
-class DatasetsAPI:
+class APIClient:
     base_url: str
     client: httpx.Client
 
-    def add(self, name: str, fpath: str):
+    def __init__(self, settings: Settings) -> None:
+        server_url = settings.check_and_get("uptrain_server_url")
+        api_key = settings.check_and_get("uptrain_access_token")
+        self.base_url = server_url.rstrip("/") + "/api/public"
+        self.client = httpx.Client(headers={"uptrain-access-token": api_key})
+
+    def check_auth(self):
+        """Ping the server to check if the client is authenticated."""
+        url = f"{self.base_url}/auth"
+        try:
+            response = self.client.get(url)
+            if not response.is_success:
+                raise RuntimeError(
+                    f"Failed to authenticate with the Uptrain server: {response.json()}"
+                )
+            else:
+                return response.json()
+        except httpx.ConnectError as e:
+            raise RuntimeError(
+                f"Failed to connect to the Uptrain server at {self.base_url}"
+            ) from e
+
+    def add_dataset(self, name: str, fpath: str):
         url = f"{self.base_url}/dataset"
         with open(fpath, "rb") as file:
             files = {"data_file": (name, file, "application/octet-stream")}
             response = self.client.post(url, data={"name": name}, files=files)
         return response.json()
 
-    def get(self, name: str, version: t.Optional[int] = None):
+    def get_dataset(self, name: str, version: t.Optional[int] = None):
         url = f"{self.base_url}/dataset"
         params: dict = {"name": name}
         if version is not None:
@@ -35,24 +55,21 @@ class DatasetsAPI:
         response = self.client.get(url, params=params)
         return response.json()
 
-    def list(self, skip: int = 0, limit: int = 100):
+    def list_datasets(self, skip: int = 0, limit: int = 100):
         url = f"{self.base_url}/datasets"
         params = {"skip": skip, "limit": limit}
         response = self.client.get(url, params=params)
         return response.json()
 
-
-@dataclass
-class ChecksetsAPI:
-    base_url: str
-    client: httpx.Client
-
-    def add(self, name: str, checkset: CheckSet):
+    def add_checkset(self, name: str, checkset: CheckSet, settings: Settings):
         url = f"{self.base_url}/checkset"
-        response = self.client.post(url, json={"name": name, "config": checkset.dict()})
+        response = self.client.post(
+            url,
+            json={"name": name, "config": checkset.dict(), "settings": settings.dict()},
+        )
         return response.json()
 
-    def get(self, name: str, version: t.Optional[int] = None):
+    def get_checkset(self, name: str, version: t.Optional[int] = None):
         url = f"{self.base_url}/checkset"
         params: dict = {"name": name}
         if version is not None:
@@ -60,19 +77,13 @@ class ChecksetsAPI:
         response = self.client.get(url, params=params)
         return response.json()
 
-    def list(self, skip: int = 0, limit: int = 100):
+    def list_checksets(self, skip: int = 0, limit: int = 10):
         url = f"{self.base_url}/checksets"
         params = {"skip": skip, "limit": limit}
         response = self.client.get(url, params=params)
         return response.json()
 
-
-@dataclass
-class RunsAPI:
-    base_url: str
-    client: httpx.Client
-
-    def add(self, dataset: str, checkset: str) -> dict:
+    def add_run(self, dataset: str, checkset: str) -> dict:
         """Schedules an evaluation on the server. Specify the dataset and checkset to use.
 
         Args:
@@ -88,7 +99,7 @@ class RunsAPI:
         )
         return response.json()
 
-    def get(self, run_id: str) -> str:
+    def get_run(self, run_id: str) -> str:
         """Get the status of a run.
 
         Args:
@@ -101,54 +112,14 @@ class RunsAPI:
         response = self.client.get(url)
         return response.json()
 
-    def list(self, only_active: bool = True):
+    def list_runs(self, num: int = 10, only_completed: bool = False):
         """List all the runs on the server.
 
         - filter by scheduled/completed/in-process?
-        - filter by timestamp?
         """
-        ...
-
-    def cancel(self, run_id: str):
-        """Cancel an active run on the server."""
-        ...
-
-    def get_logs(self, run_id: str):
-        """Get logs for a run."""
-        ...
-
-    def get_results(self, run_id: str):
-        """Get results for a run."""
-        ...
-
-
-class APIClient:
-    base_url: str
-    client: httpx.Client
-    datasets: DatasetsAPI
-    checksets: ChecksetsAPI
-    runs: RunsAPI
-
-    def __init__(self, settings: Settings) -> None:
-        server_url = settings.check_and_get("uptrain_server_url")
-        api_key = settings.check_and_get("uptrain_access_token")
-
-        self.base_url = server_url.rstrip("/") + "/api/public"
-        self.client = httpx.Client(headers={"uptrain-access-token": api_key})
-        self.datasets = DatasetsAPI(base_url=self.base_url, client=self.client)
-        self.checksets = ChecksetsAPI(base_url=self.base_url, client=self.client)
-        self.runs = RunsAPI(base_url=self.base_url, client=self.client)
-
-    def check_auth(self):
-        """Ping the server to check if the client is authenticated."""
-        url = f"{self.base_url}/auth"
-        try:
-            response = self.client.get(url)
-            if not response.is_success:
-                raise RuntimeError(
-                    f"Failed to authenticate with the Uptrain server: {response.json()}"
-                )
-        except httpx.ConnectError as e:
-            raise RuntimeError(
-                f"Failed to connect to the Uptrain server at {self.base_url}"
-            ) from e
+        url = f"{self.base_url}/runs"
+        params: dict = {"num": num}
+        if only_completed:
+            params["status"] = "completed"
+        response = self.client.get(url, params=params)
+        return response.json()
