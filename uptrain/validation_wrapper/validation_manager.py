ef7a77c3e93ba120a798d3217de3e8621b04272f
sourabhagr <108270398+sourabhagr@users.noreply.github.com>
2023-07-28 16:07:08 +0530

    Add retry logic for validation (#330)
    * Add retry logic for validation
    
    * Update validation.mdx
    
    * Fix image in validation tutorial
    
    ---------
    
    Co-authored-by: Dominastorm <dchawla228@gmail.com>

--- uptrain/validation_wrapper/validation_manager.py
+++ uptrain/validation_wrapper/validation_manager.py
@@ -10,6 +10,7 @@ from uptrain.framework.checks import Check, Settings
 class ValidationManager(BaseModel):
     check: t.Any
     completion_function: t.Any
+    retry_logic: list[dict]
     pass_condition: t.Any
     max_retries: int = 3
 
@@ -21,9 +22,12 @@ class ValidationManager(BaseModel):
             inputs[key] = [inputs[key]]
         response_is_valid = False
         num_tries = 0
+        completion_function = self.completion_function
+        response_history = {}
         while not response_is_valid:
+            response = completion_function(inputs)
+            response_history.update({'attempt_' + str(num_tries): response})
             num_tries += 1
-            response = self.completion_function(inputs)
             inputs["response"] = [response]
             data = pl.from_dict(inputs)
             data = self.check.run(data)
@@ -32,8 +36,18 @@ class ValidationManager(BaseModel):
             if response_is_valid:
                 logger.success(f"Validation check PASSED after {num_tries} attempt(s)")                
             elif num_tries < self.max_retries:
-                logger.warning(f"RETRYING validation check {num_tries} of {self.max_retries}")
+                completion_function = self.completion_function
+                selected_retry_function = "default"
+                for retry_condition in list(self.retry_logic):
+                    if retry_condition['signal'].run(data)[0] == True:
+                        completion_function = retry_condition['completion_function']
+                        selected_retry_function = retry_condition['name']
+                        break
+                logger.warning(f"RETRYING validation check {num_tries} of {self.max_retries} with logic: {selected_retry_function}")
             else:
                 response_is_valid = True
                 logger.warning(f"Validation check FAILED after {self.max_retries} attempt(s)")
+        if num_tries > 1:
+            logger.info(f"Response History: {response_history}")
+
         return response
