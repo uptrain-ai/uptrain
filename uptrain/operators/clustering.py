458faaa6d056ea0be532d24e2d4fd53b373a6f73
sourabhagr <sourabh.agrawal031994@gmail.com>
2023-10-20 08:10:53 -0700

    Add changes for topic clustering

--- uptrain/operators/clustering.py
+++ uptrain/operators/clustering.py
@@ -36,6 +36,7 @@ class Clustering(ColumnOp):
         col_in (str):  The name of the column in the DataFrame where clustering needs to be performed.
         col_out (str):  The name of the column in the DataFrame to output the assigned cluster index.
         col_out_dist (str): The name of the column in the DataFrame to output the euclidean distance from its cluster centroid.
+        col_aggs (list[str]): Optional, can be used to specify name of columns to aggregate by and run individual clustering, ex: if you want separate clustering for seperate organisations
     Example:
         ```
         import polars as pl
@@ -66,26 +67,56 @@ class Clustering(ColumnOp):
     col_in: str = 'umap_embedding'
     col_out: str = 'cluster_index'
     col_out_dist: str = 'cluster_index_distance'
+    col_aggs: list[str] = []
+    min_samples_each_cluster: int = 50
 
     def setup(self, settings: Settings):
-        if self.algorithm == "kmeans":
-            self.algorithm_obj = nltk.cluster.KMeansClusterer(self.n_clusters, distance=nltk.cluster.util.cosine_distance,avoid_empty_clusters=True)
-        else:
-            raise Exception(f"{self.algorithm} is not supported yet.")
         return self
 
     def run(self, data: pl.DataFrame) -> TYPE_TABLE_OUTPUT:      
-        embeddings = np.asarray(data[self.col_in])
-        assigned_clusters = self.algorithm_obj.cluster(embeddings, assign_clusters=True)
-        scores = []
-        for index in range(len(embeddings)):
-            scores.append(np.linalg.norm(embeddings[index] - self.algorithm_obj.means()[assigned_clusters[index]]))
+
+        self.cluster_centroids = {}
+        unique_agg_keys = ['default']
+
+        if len(self.col_aggs):
+            # First aggregate by col_aggs
+            agg_data = data.groupby(self.col_aggs).agg([pl.col(self.col_in).count().alias("num_rows_" + self.col_in)])
+            agg_data = agg_data.drop("num_rows_" + self.col_in)
+            unique_agg_keys = agg_data.to_dicts()
+
+        res_data_arr = []
+        for unique_agg_key in unique_agg_keys:
+            cond = True
+            if isinstance(unique_agg_key, dict):
+                unique_agg_key = dict([(key, unique_agg_key[key]) for key in sorted(unique_agg_key)])
+                for key,val in unique_agg_key.items():
+                    cond = cond & (data[key] == val)
+            data_subset = data.filter(cond)
+
+            n_clusters = max(1, min(self.n_clusters, int(len(data_subset)/self.min_samples_each_cluster)))
+
+            if self.algorithm == "kmeans":
+                algorithm_obj = nltk.cluster.KMeansClusterer(n_clusters, distance=nltk.cluster.util.cosine_distance, avoid_empty_clusters=True)
+            else:
+                raise Exception(f"{self.algorithm} is not supported yet.")
+
+            embeddings = np.asarray(data_subset[self.col_in])
+            assigned_clusters = algorithm_obj.cluster(embeddings, assign_clusters=True)
+            scores = []
+            for index in range(len(embeddings)):
+                scores.append(np.linalg.norm(embeddings[index] - algorithm_obj.means()[assigned_clusters[index]]))
+
+            data_subset = data_subset.with_columns([
+                        pl.Series(assigned_clusters).alias(self.col_out),
+                        pl.Series(scores).alias(self.col_out_dist),
+                        pl.Series([str(unique_agg_key)] * len(data_subset)).alias("_unique_agg_key_for_clustering")
+                ])
+            res_data_arr.append(data_subset)
+            self.cluster_centroids[str(unique_agg_key)] = [list(x.astype(np.float64)) for x in list(algorithm_obj.means())]
+
         return {
             "output": 
-                data.with_columns([
-                        pl.Series(assigned_clusters).alias(self.col_out),
-                        pl.Series(scores).alias(self.col_out_dist) 
-                ])
+                pl.concat(res_data_arr)
         }
 
 
--- uptrain/operators/language/embedding.py
+++ uptrain/operators/language/embedding.py
@@ -9,17 +9,16 @@ generated for a specified text column in a DataFrame.
 
 from __future__ import annotations
 import typing as t
+import numpy as np
 
 from loguru import logger
 import polars as pl
+import json
 
 if t.TYPE_CHECKING:
     from uptrain.framework import Settings
 from uptrain.operators.base import *
 from uptrain.utilities import lazy_load_dep
-
-InstructorEmbedding = lazy_load_dep("InstructorEmbedding", "InstructorEmbedding")
-sentence_transformers = lazy_load_dep("sentence_transformers", "sentence-transformers")
 
 
 @register_op
@@ -28,9 +27,10 @@ class Embedding(ColumnOp):
     Column operation that generates embeddings for text using pre-trained models.
 
     Attributes:
-        model (Literal["MiniLM-L6-v2", "hkunlp/instructor-xl", "mpnet-base-v2"]): The name of the pre-trained model to use.
+        model (Literal["MiniLM-L6-v2", "instructor-xl", "mpnet-base-v2", "bge-large-zh-v1.5"]): The name of the pre-trained model to use.
         col_in_text (str): The name of the text column in the DataFrame.
         col_out (str): The name of the output column in the DataFrame.
+        local_run (bool): Run clustering locally or via replicate. If set to false, should provide replicate credentials, ie set 'REPLICATE_API_TOKEN' variable in os.environ
 
     Raises:
         Exception: If the specified model is not supported.
@@ -73,35 +73,72 @@ class Embedding(ColumnOp):
 
     """
 
-    model: t.Literal["MiniLM-L6-v2", "hkunlp/instructor-xl", "mpnet-base-v2"]
+    local_run: bool = True
+    model: t.Literal["MiniLM-L6-v2", "instructor-xl", "mpnet-base-v2", "bge-large-zh-v1.5"]
     col_in_text: str = "text"
     col_out: str = "embedding"
 
     def setup(self, settings: Settings):
-        if self.model == "hkunlp/instructor-xl":
-            self._model_obj = InstructorEmbedding.INSTRUCTOR(self.model)  # type: ignore
-        elif self.model == "MiniLM-L6-v2":
-            self._model_obj = sentence_transformers.SentenceTransformer(
-                "sentence-transformers/all-MiniLM-L6-v2"
-            )  # type: ignore
-        elif self.model == "mpnet-base-v2":
-            self._model_obj = sentence_transformers.SentenceTransformer(
-                "sentence-transformers/all-mpnet-base-v2"
-            ) 
+        if self.local_run:
+            if self.model == "instructor-xl":
+                InstructorEmbedding = lazy_load_dep("InstructorEmbedding", "InstructorEmbedding")
+                self._model_obj = InstructorEmbedding.INSTRUCTOR(self.model)  # type: ignore
+            elif self.model == "MiniLM-L6-v2":
+                sentence_transformers = lazy_load_dep("sentence_transformers", "sentence-transformers")
+                self._model_obj = sentence_transformers.SentenceTransformer(
+                    "sentence-transformers/all-MiniLM-L6-v2"
+                )  # type: ignore
+            elif self.model == "mpnet-base-v2":
+                sentence_transformers = lazy_load_dep("sentence_transformers", "sentence-transformers")
+                self._model_obj = sentence_transformers.SentenceTransformer(
+                    "sentence-transformers/all-mpnet-base-v2"
+                )
+            elif self.model == "bge-large-zh-v1.5":
+                sentence_transformers = lazy_load_dep("sentence_transformers", "sentence-transformers")
+                self._model_obj = sentence_transformers.SentenceTransformer(
+                    "BAAI/bge-large-zh-v1.5"
+                )
+            else:
+                raise Exception(f"Embeddings model: {self.model} is not supported yet.")
         else:
-            raise Exception(f"Embeddings model: {self.model} is not supported yet.")
+            replicate = lazy_load_dep("replicate", "replicate")
+            self._model_obj = replicate
+            if self.model == "mpnet-base-v2":
+                self._model_url = "replicate/all-mpnet-base-v2:b6b7585c9640cd7a9572c6e129c9549d79c9c31f0d3fdce7baac7c67ca38f305"
+            elif self.model == "bge-large-zh-v1.5":
+                self._model_url = "nateraw/bge-large-en-v1.5:9cf9f015a9cb9c61d1a2610659cdac4a4ca222f2d3707a68517b18c198a9add1"
+            else:
+                raise Exception(f"Embeddings model: {self.model} is not supported yet.")
         return self
 
     def run(self, data: pl.DataFrame) -> TYPE_TABLE_OUTPUT:
         text = data.get_column(self.col_in_text)
-        if self.model == "hkunlp/instructor-xl":
-            inputs = [
-                ["Represent the developer documentation sentence: ", x] for x in text
-            ]
-        elif self.model == "MiniLM-L6-v2" or self.model == "mpnet-base-v2":
-            inputs = list(text)
+        if self.local_run:
+            if self.model == "instructor-xl":
+                inputs = [
+                    ["Represent the sentence: ", x] for x in text
+                ]
+            elif self.model == "MiniLM-L6-v2" or self.model == "mpnet-base-v2":
+                inputs = list(text)
+            else:
+                raise Exception("Embeddings model not supported")
+            results = self._model_obj.encode(inputs)
         else:
-            raise Exception("Embeddings model not supported")
-        results = self._model_obj.encode(inputs)
+            if self.model == "bge-large-zh-v1.5":
+                inputs = [
+                    ["Represent the sentence: ", x] for x in text
+                ]
+            elif self.model == "mpnet-base-v2":
+                inputs = list(text)
+            else:
+                raise Exception("Embeddings model not supported")
+            results = []
+            BATCH_SIZE = 128
+            for idx in range(int(np.ceil(len(inputs)/BATCH_SIZE))):
+                run_res = self._model_obj.run(
+                    self._model_url,
+                    input = {"text_batch": json.dumps(inputs[idx*BATCH_SIZE:(idx+1)*BATCH_SIZE])}
+                )
+                results.extend([x['embedding'] for x in run_res])
 
         return {"output": data.with_columns([pl.Series(results).alias(self.col_out)])}
