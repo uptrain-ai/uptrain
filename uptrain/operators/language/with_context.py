91b8e1c32559068524c93f3fdce0120bcb4defd7
ashish-1600 <146391040+ashish-1600@users.noreply.github.com>
2023-10-30 23:42:50 +0530

    concise response eval added (#394)
    * concise response eval added
    
    * Update with_context.py
    
    ---------
    
    Co-authored-by: sourabhagr <108270398+sourabhagr@users.noreply.github.com>

--- uptrain/operators/language/with_context.py
+++ uptrain/operators/language/with_context.py
@@ -176,7 +176,7 @@ class ContextRelevance(ColumnOp):
             for row in data.to_dicts()
         ]
         try:
-            results = self._api_client.evaluate("score_context_relevance", data_send)
+            results = self._api_client.evaluate("context_relevance", data_send)
         except Exception as e:
             logger.error(f"Failed to run evaluation for `ContextRelevance`: {e}")
             raise e
@@ -226,6 +226,52 @@ class ResponseRelevance(ColumnOp):
 
         assert results is not None
         return {"output": data.with_columns(pl.from_dicts(results))}
+
+
+
+
+@register_op
+class ResponseConciseness(ColumnOp):
+    """
+    Grades how concise the generated response is or if it has any additional irrelevant information for the question asked.
+
+    Attributes:
+        col_question (str): Column name for the stored questions
+        col_response (str): Coloumn name for the stored responses
+
+    Raises:
+        Exception: Raises exception for any failed evaluation attempts
+
+
+    """
+
+    col_question: str = "question"
+    col_response: str = "response"
+
+    def setup(self, settings: t.Optional[Settings] = None):
+        from uptrain.framework.remote import APIClient
+
+        assert settings is not None
+        self._api_client = APIClient(settings)
+        return self
+
+    def run(self, data: pl.DataFrame) -> TYPE_TABLE_OUTPUT:
+        data_send = [
+            {
+                "question": row[self.col_question],
+                "response": row[self.col_response],
+            }
+            for row in data.to_dicts()
+        ]
+        try:
+            results = self._api_client.evaluate("response_conciseness", data_send)
+        except Exception as e:
+            logger.error(f"Failed to run evaluation for `ResponseConciseness`: {e}")
+            raise e
+
+        assert results is not None
+        return {"output": data.with_columns(pl.from_dicts(results))}
+
 
 
 
