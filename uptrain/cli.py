4f756998a23680634568907e278f64ea52cc38a4
sourabhagr <sourabh.agrawal031994@gmail.com>
2023-11-09 22:59:55 -0800

    fix dep

--- uptrain/cli.py
+++ uptrain/cli.py
@@ -35,7 +35,7 @@ EXTRA_PACKAGES = {
         "rouge-score",
         # "sentence-transformers",
         # "InstructorEmbedding",
-        # "openai>=0.27",
+        "openai>=0.27",
         # "evals @ git+https://github.com/openai/evals.git",
         "replicate"
     ],
