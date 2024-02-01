eb3689508c57d4c2672460db431a6c1257179c19
Dhruv Chawla <43818888+Dominastorm@users.noreply.github.com>
2024-02-01 13:25:22 +0530

    Update remote.py

--- uptrain/framework/remote.py
+++ uptrain/framework/remote.py
@@ -577,7 +577,7 @@ class APIClient:
                 req_attrs.update([schema.sub_questions, schema.question])
             elif m in [Evals.CONTEXT_CONCISENESS]:
                 req_attrs.update([schema.question, schema.context, schema.concise_context])
-            elif m in [Evals.CONTEXT_CONCISENESS]:
+            elif m in [Evals.CONTEXT_RERANKING]:
                 req_attrs.update([schema.question, schema.context, schema.reranked_context])
             elif m in [Evals.FACTUAL_ACCURACY, Evals.RESPONSE_COMPLETENESS_WRT_CONTEXT, Evals.RESPONSE_CONSISTENCY]:
                 req_attrs.update([schema.question, schema.context, schema.response])
