9280ee7822b6403fb9633ae1709007feb0a4eaa6
ashish-1600 <ashish@uptrain.ai>
2024-03-01 16:20:54 +0530

    update evalllm.py

--- uptrain/framework/evalllm.py
+++ uptrain/framework/evalllm.py
@@ -187,7 +187,7 @@ class EvalLLM:
         if self.settings.evaluate_locally:
             results = copy.deepcopy(data)
             for idx, check in enumerate(checks):
-                if isinstance(check, ParametricEval):
+                if isinstance(check, ParametricEval) and ser_checks[idx]["check_name"] in PARAMETRIC_EVAL_TO_OPERATOR_MAPPING:
                     # Use the check_name field to get the operator and remove it from ser_checks
                     op = PARAMETRIC_EVAL_TO_OPERATOR_MAPPING[
                         ser_checks[idx].pop("check_name")
