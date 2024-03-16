2438d5cb8010792e9b40632ae88574ba864eada1
Shreyansh Jain <shrjain1111@gmail.com>
2024-03-16 13:19:07 +0530

    1. better handling of ports 2. added project name 3. port closes at keyboard interrupt 4. add method to install 5. add method to run custom promptfoo command 6. better handling of promptfoo version

--- uptrain/integrations/promptfoo_utils.py
+++ uptrain/integrations/promptfoo_utils.py
@@ -5,6 +5,7 @@ from uptrain import Evals
 import socket
 
 DEFAULT_PORT_PROMPTFOO = 15500
+PROMPTFOO_version = "latest"
 """
 evals: [Evals.Metric1, Evals.Metric2]
 weight: [Weight1, Weight2]
@@ -55,7 +56,7 @@ def tempalate_uptrain_eval():
     data = [{"question": input_question, "context": input_context, "response": output}]
     settings = Settings(openai_api_key=os.environ["OPENAI_API_KEY"], model=eval_model)
     eval_llm = EvalLLM(settings=settings)
-    results = eval_llm.evaluate(project_name=__project_name__,data=data, checks=eval_type)
+    results = eval_llm.evaluate(project_name = (project_name), data=data, checks=eval_type)
     if results[0][score_var] > threshold_var:
         return {
             "pass": True,
@@ -96,8 +97,7 @@ def format_uptrain_template(data, evals_compiled, threshold, eval_model, project
     )
     uptrain_template = uptrain_template.replace("threshold_var", "{}".format(threshold))
     uptrain_template = uptrain_template.replace("eval_model", "'{}'".format(eval_model))
-    uptrain_template = uptrain_template.replace("__project_name__", "'{}'".format(project_name))
-    
+    uptrain_template = uptrain_template.replace("(project_name)", "'{}'".format(project_name + "("+ evals_compiled["eval_type"] +")"))
     return uptrain_template
 
 
@@ -110,7 +110,7 @@ def generate_promptfoo_yaml_file(py_obj):
     try:
         with open("promptfooconfig.yaml", "w") as f:
             yaml.dump(py_obj, f, sort_keys=False)
-            logger.success("Succesfully generated file: promptfooconfig.yaml")
+            # logger.success("Succesfully generated file: promptfooconfig.yaml")
     except:
         logger.error("Unable to generate file: promptfooconfig.yaml")
 
@@ -120,17 +120,23 @@ Used to check for an open port
 """
 
 
+
 def generate_open_port():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port = DEFAULT_PORT_PROMPTFOO
     port_res = sock.connect_ex(
         ("localhost", DEFAULT_PORT_PROMPTFOO)
-    )  # if 0 then default: DEFAULT_PORT_PROMPTFOO already in use
-    if port_res == 0:
-        logger.info("Default port " + str(DEFAULT_PORT_PROMPTFOO) + " already in use")
+    )                                       # if 0 then default: DEFAULT_PORT_PROMPTFOO already in use
+    
+    while port_res == 0 and port>0:
+        port = port - 1
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        port_res = sock.connect_ex(
+            ("localhost", port)
+        )
+    
+    if port_res==0:
         sock = socket.socket()
         sock.bind(("", 0))
-        open_port = sock.getsockname()[1]
-        logger.info("Using port: " + str(open_port))
-    else:
-        open_port = 15500
-    return open_port
+        port = sock.getsockname()[1]
+    return port    
