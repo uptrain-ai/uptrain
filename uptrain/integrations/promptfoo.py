db30de63cf391837c1b21efd1255a38291078f81
shrjain1312 <36454110+shrjain1312@users.noreply.github.com>
2024-03-16 14:10:09 +0530

    Promptfoo Integration Updates (#640)
    * Added project_name for uptrain evals to avoid multiple dashboards
    
    * 1. better handling of ports
    2. added project name
    3. port closes at keyboard interrupt
    4. add method to install
    5. add method to run custom promptfoo command
    6. better handling of promptfoo version
    
    * updated notebook
    
    * minor changes in notebook
    
    * minor edits in notebook
    
    * Run black
    
    ---------
    
    Co-authored-by: Dhruv Chawla <dhruv@uptrain.ai>

--- uptrain/integrations/promptfoo.py
+++ uptrain/integrations/promptfoo.py
@@ -16,6 +16,14 @@ class EvalPromptfoo:
     def __init__(self) -> None:
         return None
 
+    def install(self):
+        try:
+            subprocess.run(["npm", "install", "promptfoo@" + pr_u.PROMPTFOO_version])
+            logger.success("Install Successfully")
+        except Exception as e:
+            logger.error("Error installing Promptfoo:" + e)
+            logger.error("Try running npx promptfoo@" + pr_u.PROMPTFOO_version)
+
     def evaluate(
         self,
         evals_list: t.Union[list],
@@ -24,92 +32,123 @@ class EvalPromptfoo:
         threshold: t.Union[float],
         prompts: t.Union[str],
         providers: t.Union[list],
-        redirect_webview: t.Optional[bool] = False,
         output_file: t.Optional[str] = "results_" + timestr + ".csv",
         port: t.Optional[int] = pr_u.generate_open_port(),
         eval_model: t.Optional[str] = "gpt-3.5-turbo-1106",
-    ):
-        evals_compiled = pr_u.compile_evals(evals_list, evals_weight)
+        project_name: t.Optional[str] = "EvalPromptfoo " + str(timestr),
+        kill_process_on_exit: t.Optional[bool] = True,
+    ) -> None:
+        try:
+            evals_compiled = pr_u.compile_evals(evals_list, evals_weight)
+            if isinstance(input_data, pl.DataFrame):
+                input_data = input_data.to_dicts()
+            elif isinstance(input_data, pd.DataFrame):
+                input_data = input_data.to_dict(orient="records")
+            yaml_data = {
+                "prompts": prompts,
+                "providers": providers,
+                "tests": [
+                    {
+                        "description": "Data " + str(i + 1),
+                        "vars": {
+                            "question": input_data[i]["question"],
+                            "context": input_data[i]["context"],
+                        },
+                        "threshold": threshold,
+                        "assert": [
+                            {
+                                "type": "python",
+                                "value": pr_u.format_uptrain_template(
+                                    input_data[i],
+                                    evals_compiled[j],
+                                    threshold,
+                                    eval_model,
+                                    project_name,
+                                ),
+                            }
+                            for j in range(len(evals_compiled))
+                        ],
+                    }
+                    for i in range(len(input_data))
+                ],
+            }
+            pr_u.generate_promptfoo_yaml_file(yaml_data)
+            try:
+                subprocess.run(
+                    [
+                        "npx",
+                        "--yes",
+                        "promptfoo@" + pr_u.PROMPTFOO_version,
+                        "eval",
+                        "-o",
+                        output_file,
+                    ]
+                )
+                logger.success("Evaluations successfully generated")
+                logger.success("Saved results to file: " + output_file)
+                try:
+                    subprocess.run(
+                        [
+                            "npx",
+                            "promptfoo@" + pr_u.PROMPTFOO_version,
+                            "view",
+                            "-y",
+                            "-p",
+                            str(port),
+                        ]
+                    )
+                    subprocess.run(
+                        ["npx", "promptfoo@latest", "view", "-y", "-p", str(port)]
+                    )
+                except Exception as e:
+                    logger.error(
+                        "Something failed: Try running !npx promptfoo@latest view" + e
+                    )
 
-        if isinstance(input_data, pl.DataFrame):
-            input_data = input_data.to_dicts()
-        elif isinstance(input_data, pd.DataFrame):
-            input_data = input_data.to_dict(orient="records")
+            except Exception as e:
+                logger.error(f"Evaluation failed with error: {e}")
+                raise e
+        except:
+            if kill_process_on_exit:
+                try:
+                    pid = subprocess.check_output(["lsof", "-t", "-i:" + str(port)])
+                    pids_str = pid.decode()
+                    pids = [
+                        int(pid)
+                        for pid in pids_str.split("\n")
+                        if pid.strip().isdigit()
+                    ]
+                    try:
+                        for pid in pids:
+                            subprocess.run(["kill", str(pid)])
+                        print(
+                            f"Closed application at http://localhost:{port} successfully"
+                        )
+                        print(f"To reopen it try running view method in EvalPromptfoo")
+                    except subprocess.CalledProcessError as e:
+                        pass
+                except:
+                    print("No running process to close")
+            else:
+                return None
 
-        yaml_data = {
-            "prompts": prompts,
-            "providers": providers,
-            "tests": [
-                {
-                    "description": "Data " + str(i + 1),
-                    "vars": {
-                        "question": input_data[i]["question"],
-                        "context": input_data[i]["context"],
-                    },
-                    "threshold": threshold,
-                    "assert": [
-                        {
-                            "type": "python",
-                            "value": pr_u.format_uptrain_template(
-                                input_data[i], evals_compiled[j], threshold, eval_model
-                            ),
-                        }
-                        for j in range(len(evals_compiled))
-                    ],
-                }
-                for i in range(len(input_data))
-            ],
-        }
-
-        pr_u.generate_promptfoo_yaml_file(yaml_data)
+    def view(self, port: t.Optional[int] = pr_u.generate_open_port()):
         try:
             subprocess.run(
-                ["npx", "--yes", "promptfoo@latest", "eval", "-o", output_file]
+                [
+                    "npx",
+                    "promptfoo@" + pr_u.PROMPTFOO_version,
+                    "view",
+                    "-y",
+                    "-p",
+                    str(port),
+                ]
             )
-            logger.success("Evaluations successfully generated")
-            logger.success("Saved results to file: " + output_file)
-            if redirect_webview:
-                try:
-                    if isinstance(port, int):
-                        subprocess.run(
-                            ["npx", "promptfoo@latest", "view", "-y", "-p", str(port)]
-                        )
-                        logger.success(
-                            "Successfully rerouted to promptfoo dashboards @ http://localhost:"
-                            + str(port)
-                            + "/"
-                        )
-                    else:
-                        subprocess.run(["npx", "promptfoo@latest", "view", "-y"])
-                        logger.success(
-                            "Successfully rerouted to promptfoo dashboards @ http://localhost:"
-                            + str(pr_u.DEFAULT_PORT_PROMPTFOO)
-                        )
-                except Exception as e:
-                    logger.error(f"Failed to generate a view: {e}")
-            else:
-                try:
-                    if isinstance(port, int):
-                        subprocess.run(
-                            ["npx", "promptfoo@latest", "view", "-p", str(port)]
-                        )
-                        logger.success(
-                            "Open "
-                            + str(port)
-                            + "/"
-                            + " in webbrowser to view dashboards"
-                        )
-                    else:
-                        subprocess.run(["npx", "promptfoo@latest", "view"])
-                        logger.success(
-                            "Open http://localhost:"
-                            + str(pr_u.DEFAULT_PORT_PROMPTFOO)
-                            + "/"
-                            + " in webbrowser to view dashboards"
-                        )
-                except Exception as e:
-                    logger.error(f"Failed to generate a view: {e}")
         except Exception as e:
-            logger.error(f"Evaluation failed with error: {e}")
-            raise e
-        return None
+            logger.error("Something failed: Try running !npx promptfoo@latest view" + e)
+
+    def custom(self, command: t.Union[str] = "init"):
+        try:
+            subprocess.run(["npx", "promptfoo@" + pr_u.PROMPTFOO_version, command])
+        except Exception as e:
+            logger.error("Something failed: " + e)
--- uptrain/integrations/promptfoo_utils.py
+++ uptrain/integrations/promptfoo_utils.py
@@ -5,6 +5,7 @@ from uptrain import Evals
 import socket
 
 DEFAULT_PORT_PROMPTFOO = 15500
+PROMPTFOO_version = "latest"
 """
 evals: [Evals.Metric1, Evals.Metric2]
 weight: [Weight1, Weight2]
@@ -55,7 +56,9 @@ def tempalate_uptrain_eval():
     data = [{"question": input_question, "context": input_context, "response": output}]
     settings = Settings(openai_api_key=os.environ["OPENAI_API_KEY"], model=eval_model)
     eval_llm = EvalLLM(settings=settings)
-    results = eval_llm.evaluate(data=data, checks=eval_type)
+    results = eval_llm.evaluate(
+        project_name=(project_name), data=data, checks=eval_type
+    )
     if results[0][score_var] > threshold_var:
         return {
             "pass": True,
@@ -75,7 +78,7 @@ func(format_uptrain_template): Adds individual variables to the template
 """
 
 
-def format_uptrain_template(data, evals_compiled, threshold, eval_model):
+def format_uptrain_template(data, evals_compiled, threshold, eval_model, project_name):
     uptrain_template_lines = inspect.getsourcelines(tempalate_uptrain_eval)[0]
     uptrain_template = "".join(uptrain_template_lines[1:])
 
@@ -96,6 +99,10 @@ def format_uptrain_template(data, evals_compiled, threshold, eval_model):
     )
     uptrain_template = uptrain_template.replace("threshold_var", "{}".format(threshold))
     uptrain_template = uptrain_template.replace("eval_model", "'{}'".format(eval_model))
+    uptrain_template = uptrain_template.replace(
+        "(project_name)",
+        "'{}'".format(project_name + "(" + evals_compiled["eval_type"] + ")"),
+    )
     return uptrain_template
 
 
@@ -108,7 +115,6 @@ def generate_promptfoo_yaml_file(py_obj):
     try:
         with open("promptfooconfig.yaml", "w") as f:
             yaml.dump(py_obj, f, sort_keys=False)
-            logger.success("Succesfully generated file: promptfooconfig.yaml")
     except:
         logger.error("Unable to generate file: promptfooconfig.yaml")
 
@@ -120,15 +126,18 @@ Used to check for an open port
 
 def generate_open_port():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    port = DEFAULT_PORT_PROMPTFOO
     port_res = sock.connect_ex(
         ("localhost", DEFAULT_PORT_PROMPTFOO)
     )  # if 0 then default: DEFAULT_PORT_PROMPTFOO already in use
+
+    while port_res == 0 and port > 0:
+        port = port - 1
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        port_res = sock.connect_ex(("localhost", port))
+
     if port_res == 0:
-        logger.info("Default port " + str(DEFAULT_PORT_PROMPTFOO) + " already in use")
         sock = socket.socket()
         sock.bind(("", 0))
-        open_port = sock.getsockname()[1]
-        logger.info("Using port: " + str(open_port))
-    else:
-        open_port = 15500
-    return open_port
+        port = sock.getsockname()[1]
+    return port
