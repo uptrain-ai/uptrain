a78da46acd5eeb51214394b8111f7203c0134ee3
dhruv-macbook-air-m2 <dhruv@uptrain.ai>
2024-04-16 13:14:54 +0530

    Change default model to gpt-3.5-turbo

--- uptrain/framework/base.py
+++ uptrain/framework/base.py
@@ -73,7 +73,7 @@ class Settings(BaseSettings):
     )
 
     # LLM model to run the evaluations
-    model: str = "gpt-3.5-turbo-1106"
+    model: str = "gpt-3.5-turbo"
     seed: t.Union[int, None] = None
     response_format: t.Union[dict, None] = None
 
