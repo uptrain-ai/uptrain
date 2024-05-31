5f9be84fe582c3789ca1d928e7f26ad276d6a5c2
Dhruv Chawla <43818888+Dominastorm@users.noreply.github.com>
2024-05-31 16:26:41 +0530

    Add documentation and tutorials for new conversation evals (#710)
    * Add tutorial and documentation for guideline
     adherence operator
    
    * Update conversation satisfaction documentation
    
    * Add documentation and tutorial for conversation number of turns
    
    * Fixes
    
    * Update imports
    
    * Fix score mapping in query resolution operator
    
    * Update params in tutorial
    
    * Add documentation and tutorial for query resolution
    
    * Update mint.json

--- uptrain/operators/language/conversation.py
+++ uptrain/operators/language/conversation.py
@@ -233,7 +233,7 @@ class QueryResolutionScore(ColumnOp):
     user_role: str = "User"
     assistant_role: str = "Assistant"
     scenario_description: t.Optional[str] = None
-    score_mapping: dict = {"A": 0.0, "B": 0.5, "C": 1.0}
+    score_mapping: dict = {"A": 1.0, "B": 0.5, "C": 0.0}
 
     def setup(self, settings: t.Optional[Settings] = None):
         from uptrain.framework.remote import APIClient
